5758f59ad76685f54cc438180aa4cc6be4a4b20e
dkdhirendra <90185036+dkdhirendra@users.noreply.github.com>
2021-11-26 23:41:25 +0530

    js assignment

---
+++ abc.js
@@ -0,0 +1,29 @@
+var Person1 = new Object()
+Person1.FirstName = "Ravi",
+    Person1.LastName = "Mishra",
+    Person1.OfficeAddress = "noida,Uttar Pradesh"
+
+var Person2 = new Object()
+Person2.FirstName = "Ayush",
+    Person2.LastName = "kumar",
+    Person2.OfficeAddress = "Boring road,Patna"
+
+var Person3 = new Object()
+Person3.FirstName = "Ashish",
+    Person3.LastName = "Anand",
+    Person3.OfficeAddress = "Gurugram,New Delhi"
+
+console.log("Person1:");
+console.log("FirstName: " + Person1.FirstName);
+console.log("LastName: " + Person1.LastName);
+console.log("OfficeAddress: " + Person1.OfficeAddress);
+
+console.log("Person2:");
+console.log("FirstName: " + Person2.FirstName);
+console.log("LastName: " + Person2.LastName);
+console.log("OfficeAddress: " + Person2.OfficeAddress);
+
+console.log("Person3:");
+console.log("FirstName: " + Person3.FirstName);
+console.log("LastName: " + Person3.LastName);
+console.log("OfficeAddress: " + Person3.OfficeAddress);
